f78895becdb5fbf6f5ad4ad49c0cc80038b01933
Alessandra Silva <alessandra.apds@gmail.com>
2023-03-15 22:20:35 +0100

    users router

--- Routers/usersRouter.js
+++ Routers/usersRouter.js
@@ -12,4 +12,47 @@ router.get("/", (req, res) => {
     .catch((e) => res.sendStatus(500));
 });
 
+// Getting an unique user per id
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  pool
+    .query("SELECT * FROM users WHERE id=$1;", [id])
+    .then(({ rows }) => res.json(rows))
+    .catch((e) => res.sendStatus(500));
+});
+
+// Inserting a new user into the database
+router.post("/", (req, res) => {
+  const { first_name, last_name, age, active } = req.body;
+  pool
+    .query(
+      "INSERT INTO users (first_name, last_name, age, active) VALUES ($1, $2, $3, $4) RETURNING *",
+      [first_name, last_name, age, active]
+    )
+    .then(({ rows }) => res.json(rows))
+    .catch((e) => res.sendStatus(500));
+});
+
+// Updating a user
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const { last_name } = req.body;
+  pool
+    .query("UPDATE users SET last_name=$1 WHERE id=$2 RETURNING *", [
+      last_name,
+      id,
+    ])
+    .then(({ rows }) => res.json(rows))
+    .catch((e) => res.sendStatus(500));
+});
+
+// Deleting a user
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  pool
+    .query("DELETE * FROM users WHERE id=$1 RETURNING *", [id])
+    .then(({ rows }) => res.json(rows))
+    .catch((e) => res.sendStatus(500));
+});
+
 module.exports = router;
--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 8001;
 
 const users = require("./Routers/usersRouter");
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.json());
 app.use("/api/users", users);
 app.listen(PORT, () => {
   console.log(`The server is listening to ${PORT}`);
